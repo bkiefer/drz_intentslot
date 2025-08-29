34c8a6608117b3a748a69065eacdb7cbee23ffd4
Bernd Kiefer <j_a_w_bone@gmx.de>
2025-08-29 19:29:53 +0200

    Add missing BERT base model

--- model_download.sh
+++ model_download.sh
@@ -4,6 +4,7 @@ scrdir=`dirname "$0"`
 if test -d adapters; then
     :
 else
+    git clone https://huggingface.co/bert-base-german-cased
     # Download models for intent and slot recognition
     wget https://cloud.dfki.de/owncloud/index.php/s/RW6f56AwiqgBKem/download/models.tar.gz
     tar xf models.tar.gz && rm models.tar.gz
