468914e2f4db153e79d05de91de06ff3d0803384
Bernd Kiefer <j_a_w_bone@gmx.de>
2025-09-22 12:48:47 +0200

    Check for git-lfs availability

--- model_download.sh
+++ model_download.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #set -x
 scrdir=`dirname "$0"`
-if test -d adapters; then
-    :
-else
+type git-lfs >/dev/null 2>&1 || \
+    (echo "No git-lfs installed, aborting!" ; exit 1)
+if test \! -d bert-base-german-cased; then
     git clone https://huggingface.co/bert-base-german-cased
-    # Download models for intent and slot recognition
+fi
+if test \! -d adapters; then
+    # Download adapters for intent and slot recognition
     wget https://cloud.dfki.de/owncloud/index.php/s/RW6f56AwiqgBKem/download/models.tar.gz
     tar xf models.tar.gz && rm models.tar.gz
 fi
